f4a8edfaa6261e9bf750f1397d3d7be3272dc558
민경원 <min-kyeongwon@mingyeong-won-ui-noteubug.local>
2023-09-28 20:39:18 +0900

    aslkfje

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -6,55 +6,57 @@ import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import styles from "./banner.module.css";
 
 export default function Sample() {
-  const slideRef = useRef<HTMLDivElement>(null);
-
-  const handleClickNext = () => {
-    let items = slideRef.current?.querySelectorAll(".item");
-    if(items) slideRef.current?.appendChild(items[0]);
-  };
-
-  const handleClickPrev = () => {
-    let items = slideRef.current?.querySelectorAll(".item");
-    if(items) slideRef.current?.prepend(items[items.length - 1]);
-  };
-
-  const data = [
+  const [items, setItems] = useState([
     {
-      id: 1,
+      id: 0,
       imgUrl: "/banner01.jpg",
       desc: "Some beautiful roads cannot be discovered without getting loss.",
       name: "EXPLORE NATURE",
     },
     {
-      id: 2,
+      id: 1,
       imgUrl: "/banner02.jpg",
       desc: "Some beautiful roads cannot be discovered without getting loss.",
       name: "EXPLORE NATURE",
     },
     {
-      id: 3,
+      id: 2,
       imgUrl: "/banner03.jpg",
       desc: "Some beautiful roads cannot be discovered without getting loss.",
       name: "EXPLORE NATURE",
     },
     {
-      id: 4,
+      id: 3,
       imgUrl: "/banner04.jpg",
       desc: "Some beautiful roads cannot be discovered without getting loss.",
       name: "EXPLORE NATURE",
     },
     {
-      id: 5,
-      imgUrl:"/banner05.jpg",
+      id: 4,
+      imgUrl: "/banner05.jpg",
       desc: "Some beautiful roads cannot be discovered without getting loss.",
       name: "EXPLORE NATURE",
-    },
-  ];
+    }
+  ]); 
+
+  const handleClickNext = () => {
+    let temp = items;
+    temp.push(items[0]);
+    temp.shift();
+    setItems(temp);
+  };
+
+  const handleClickPrev = () => {
+    let temp = items;
+    temp.unshift(items[items.length-1]);
+    temp.pop();
+    setItems(temp);
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.slide} ref={slideRef}>
-        {data.map((item) => (
+      <div className={styles.slide}>
+        {items.map((item) => {return(
           <div
             key={item.id}
             className={styles.item}
@@ -66,7 +68,7 @@ export default function Sample() {
               <button>See more</button>
             </div>
           </div>
-        ))}
+        )})}
       </div>
       <div className={styles.buttons}>
         <button id="prev" onClick={handleClickPrev}>
